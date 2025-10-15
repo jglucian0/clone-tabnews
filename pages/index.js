7771ba2d68ec4cd7340c8d164423ecfa22f9181d
João Gabriel Luciano <jglucianoi@icloud.com>
2025-10-14 23:21:09 -0300

    Add scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <div>Você é o meu melhor acaso e a minha melhor escolha de todos os dias. Se me ama, da uma risadinha 😊</div>;
+  return (
+    <div>
+      Você é o meu melhor acaso e a minha melhor escolha de todos os dias. Se me
+      ama, da uma risadinha 😊
+    </div>
+  );
 }
 
 export default Home;
