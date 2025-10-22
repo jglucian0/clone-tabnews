8c698ce879edd3ab486215226901a5e52097493b
João Gabriel Luciano <jglucianoi@icloud.com>
2025-10-21 21:07:24 -0300

    Add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,7 +3,11 @@
 // Método .send() não envia o charset (tipo de teclado utilizado) automaticamente
 // Método .json() envia o charset automaticamente (utf-8)
 
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 as sum;');
+  console.log(result.rows);
   response.status(200).send({ chave: "valor" });
 }
 
