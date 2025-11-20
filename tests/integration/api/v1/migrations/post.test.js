1e8d8bbd1e4799f8cd615d1cac32da3b2f677b4e
João Gabriel Luciano <jglucianoi@icloud.com>
2025-11-20 01:17:33 -0300

    feat: add `posttest` script

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,29 +1,41 @@
-import database from "infra/database.js";
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await database.query("drop schema public cascade; create schema public;");
+  await orchestrator.clearDatabase();
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const responseA = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(201);
+
+        const responseBody = await response.json();
+
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
+      test("For the second time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(200);
+
+        const responseBody = await response.json();
+
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBe(0);
+      });
+    });
   });
-  expect(responseA.status).toBe(201);
-
-  const responseABody = await responseA.json();
-
-  expect(Array.isArray(responseABody)).toBe(true);
-  expect(responseABody.length).toBeGreaterThan(0);
-
-  const responseB = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(responseB.status).toBe(200);
-
-  const responseBBody = await responseB.json();
-
-  expect(Array.isArray(responseBBody)).toBe(true);
-  expect(responseBBody.length).toBe(0);
 });
