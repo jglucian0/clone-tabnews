93fc6e208c4695852fc61c0eb7985d990f6109ba
João Gabriel Luciano <jglucianoi@icloud.com>
2025-12-16 23:59:59 -0300

    feat: use `ServiceError` in `databse.js`

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
 const sslConfig =
   process.env.POSTGRES_HOST === "localhost"
@@ -13,9 +14,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log("\n Erro dentro do catch do database.js:");
-    console.error(error);
-    throw error;
+    const serviceErrorObject = new ServiceError({
+      message: "Erro na conexão com o Banco ou na Query.",
+      cause: error,
+    });
+    throw serviceErrorObject;
   } finally {
     await client?.end();
   }
