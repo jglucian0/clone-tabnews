11bd085ead092369b033e3d1eb846c655864c245
João Gabriel Luciano <jglucianoi@icloud.com>
2026-05-07 22:44:05 -0300

    feat: implement session deletion

--- models/session.js
+++ models/session.js
@@ -82,10 +82,34 @@ async function renew(sessionId) {
   }
 }
 
+async function expireById(sessionId) {
+  const expiresSessionObject = await runUpdateQuery(sessionId);
+  return expiresSessionObject;
+
+  async function runUpdateQuery(sessionId) {
+    const results = await database.query({
+      text: `UPDATE
+              sessions
+            SET
+              expires_at = expires_at - interval '1 year',
+              updated_at = NOW()
+            WHERE
+              id = $1
+            RETURNING
+              *
+            ;`,
+      values: [sessionId],
+    });
+
+    return results.rows[0];
+  }
+}
+
 const session = {
   create,
   findOneValidByToken,
   renew,
+  expireById,
   EXPIRATION_IN_MILLISECONDS,
 };
 
